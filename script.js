dce1bb99124cf95221a939198ffe68b56e5afedc
Rodrigo <roviol@gmail.com>
2023-06-09 08:45:10 -0300

    feat: scroll animation

--- script.js
+++ script.js
@@ -64,6 +64,30 @@ function initScrollSuave() {
   });
 }
 
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowHalf = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowHalf < 0;
+
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    window.addEventListener("scroll", animaScroll);
+  }
+
+  animaScroll();
+}
+
 initTabNav();
 initAccordion();
 initScrollSuave();
+initAnimacaoScroll();
